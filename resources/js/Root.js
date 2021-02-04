7b0c27f2db5c6be31a661868acab2228d9c1dedf
VICTOR-TVL <vheecthurikuomola@gmail.com>
2021-02-04 14:15:29 +0100

    Laravel-React setup

---
+++ resources/js/Root.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { User } from "./components/User";
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <User />
+      </div>
+    );
+  }
+}
+
+
+export default App;
